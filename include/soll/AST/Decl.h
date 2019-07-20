87e9a5c4ae7f9a9404046de029f1f9dfa41654db
WuDaiYang <dywu@skymizer.com>
2019-07-20 23:20:13 +0800

    [AST] rename some Decl nodes
    - ParameterList         -> ParamList
    - ContractDefinition    -> ContractDecl
    - FunctionDefinition    -> FunctionDecl
    - VariableDeclaration   -> VarDecl
    - CallableVarDeclration -> CallableVarDecl

--- include/soll/AST/Decl.h
+++ include/soll/AST/Decl.h
@@ -40,23 +40,23 @@ public:
 };
 
 class InheritanceSpecifier;
-class FunctionDefinition;
-class ContractDefinition : public Decl {
+class FunctionDecl;
+class ContractDecl : public Decl {
 public:
   enum class ContractKind { Interface, Contract, Library };
 
 private:
   std::vector<std::unique_ptr<InheritanceSpecifier>> BaseContracts;
-  std::vector<std::unique_ptr<FunctionDefinition>> Functions;
+  std::vector<std::unique_ptr<FunctionDecl>> Functions;
   ContractKind Kind;
 
 public:
-  ContractDefinition(llvm::StringRef name,
+  ContractDecl(llvm::StringRef name,
       std::vector<std::unique_ptr<InheritanceSpecifier>> &&baseContracts,
       ContractKind kind = ContractKind::Contract)
       : Decl(name), BaseContracts(std::move(baseContracts)),
         Kind(kind) {}
-  virtual ~ContractDefinition() override = default;
+  virtual ~ContractDecl() override = default;
 };
 
 class InheritanceSpecifier {
@@ -70,24 +70,24 @@ public:
         Arguments(std::move(arguments)) {}
 };
 
-class ParameterList;
-class CallableDeclaration : public Decl {
-  std::unique_ptr<ParameterList> Parameters;
-  std::unique_ptr<ParameterList> ReturnParameters;
+class ParamList;
+class CallableVarDecl : public Decl {
+  std::unique_ptr<ParamList> Params;
+  std::unique_ptr<ParamList> ReturnParams;
 
 public:
-  CallableDeclaration(llvm::StringRef name,
-      Visibility visibility, std::unique_ptr<ParameterList> &&parameters,
-      std::unique_ptr<ParameterList> &&returnParameters = nullptr)
-      : Decl(name, visibility), Parameters(std::move(parameters)),
-        ReturnParameters(std::move(returnParameters)) {}
+  CallableVarDecl(llvm::StringRef name,
+      Visibility visibility, std::unique_ptr<ParamList> &&Params,
+      std::unique_ptr<ParamList> &&returnParams = nullptr)
+      : Decl(name, visibility), Params(std::move(Params)),
+        ReturnParams(std::move(returnParams)) {}
 };
 
 enum class StateMutability { Pure, View, NonPayable, Payable };
 class ModifierInvocation;
 class Block;
 
-class FunctionDefinition : public CallableDeclaration {
+class FunctionDecl : public CallableVarDecl {
   StateMutability SM;
   bool IsConstructor;
   std::vector<std::unique_ptr<ModifierInvocation>> FunctionModifiers;
@@ -95,29 +95,29 @@ class FunctionDefinition : public CallableDeclaration {
   bool Implemented;
 
 public:
-  FunctionDefinition(llvm::StringRef name,
+  FunctionDecl(llvm::StringRef name,
       Visibility visibility, StateMutability sm, bool isConstructor,
-      std::unique_ptr<ParameterList> &&parameters,
+      std::unique_ptr<ParamList> &&Params,
       std::vector<std::unique_ptr<ModifierInvocation>> &&modifiers,
-      std::unique_ptr<ParameterList> &&returnParameters,
+      std::unique_ptr<ParamList> &&returnParams,
       std::unique_ptr<Block> &&body)
-      : CallableDeclaration(name, visibility, std::move(parameters),
-                            std::move(returnParameters)),
+      : CallableVarDecl(name, visibility, std::move(Params),
+                        std::move(returnParams)),
         SM(sm), IsConstructor(isConstructor),
         FunctionModifiers(std::move(modifiers)), Body(std::move(body)),
         Implemented(body != nullptr) {}
 };
 
-class VariableDeclaration;
-class ParameterList {
-  std::vector<std::unique_ptr<VariableDeclaration>> Parameters;
+class VarDecl;
+class ParamList {
+  std::vector<std::unique_ptr<VarDecl>> Params;
 
 public:
-  ParameterList(std::vector<std::unique_ptr<VariableDeclaration>> &&parameters)
-      : Parameters(std::move(parameters)) {}
+  ParamList(std::vector<std::unique_ptr<VarDecl>> &&Params)
+      : Params(std::move(Params)) {}
 };
 
-class VariableDeclaration : public Decl {
+class VarDecl : public Decl {
 public:
   enum class Location { Unspecified, Storage, Memory, CallData };
 
@@ -130,11 +130,11 @@ private:
   Location ReferenceLocation;
 
 public:
-  VariableDeclaration(llvm::StringRef type, llvm::StringRef name,
-                      std::unique_ptr<Expr> &&value, Visibility visibility,
-                      bool isStateVar = false, bool isIndexed = false,
-                      bool isConstant = false,
-                      Location referenceLocation = Location::Unspecified)
+  VarDecl(llvm::StringRef type, llvm::StringRef name,
+          std::unique_ptr<Expr> &&value, Visibility visibility,
+          bool isStateVar = false, bool isIndexed = false,
+          bool isConstant = false,
+          Location referenceLocation = Location::Unspecified)
       : Decl(name, visibility), TypeName(type),
         Value(std::move(value)), IsStateVariable(isStateVar),
         IsIndexed(isIndexed), IsConstant(isConstant),
