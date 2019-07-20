c519f2deadd504962b045450c5f2cda43653f393
WuDaiYang <dywu@skymizer.com>
2019-07-20 16:06:52 +0800

    [AST] remove SourceLocation in Decl

--- include/soll/AST/Decl.h
+++ include/soll/AST/Decl.h
@@ -15,7 +15,6 @@ public:
   enum class Visibility { Default, Private, Internal, Public, External };
 
 private:
-  SourceLocation Loc;
   Visibility Vis;
   std::string Name;
 
@@ -23,38 +22,21 @@ protected:
   friend class ASTReader;
 
 protected:
-  Decl(SourceLocation L, llvm::StringRef Name,
+  Decl(llvm::StringRef Name,
        Visibility vis = Visibility::Default)
-      : Loc(L), Name(Name.str()), Vis(vis) {}
+      : Name(Name.str()), Vis(vis) {}
   virtual ~Decl();
 
-public:
-  virtual SourceRange getSourceRange() const LLVM_READONLY {
-    return SourceRange(getLocation(), getLocation());
-  }
-
-  SourceLocation getBeginLoc() const LLVM_READONLY {
-    return getSourceRange().getBegin();
-  }
-
-  SourceLocation getEndLoc() const LLVM_READONLY {
-    return getSourceRange().getEnd();
-  }
-
-  SourceLocation getLocation() const { return Loc; }
-  void setLocation(SourceLocation L) { Loc = L; }
 };
 
 class PragmaDirective {
-  SourceLocation Loc;
   std::vector<Token> Tokens;
   std::vector<std::string> Literals;
 
 public:
-  PragmaDirective(SourceLocation const &location,
-                  std::vector<Token> const &tokens,
+  PragmaDirective(std::vector<Token> const &tokens,
                   std::vector<std::string> const &literals)
-      : Loc(location), Tokens(tokens), Literals(literals) {}
+      : Tokens(tokens), Literals(literals) {}
 };
 
 class InheritanceSpecifier;
@@ -69,24 +51,22 @@ private:
   ContractKind Kind;
 
 public:
-  ContractDefinition(
-      const SourceLocation &location, llvm::StringRef name,
+  ContractDefinition(llvm::StringRef name,
       std::vector<std::unique_ptr<InheritanceSpecifier>> &&baseContracts,
       ContractKind kind = ContractKind::Contract)
-      : Decl(location, name), BaseContracts(std::move(baseContracts)),
+      : Decl(name), BaseContracts(std::move(baseContracts)),
         Kind(kind) {}
   virtual ~ContractDefinition() override = default;
 };
 
 class InheritanceSpecifier {
-  SourceLocation Loc;
   std::string BaseName;
   std::vector<std::unique_ptr<Expr>> Arguments;
 
 public:
-  InheritanceSpecifier(const SourceLocation &location, llvm::StringRef baseName,
+  InheritanceSpecifier(llvm::StringRef baseName,
                        std::vector<std::unique_ptr<Expr>> &&arguments)
-      : Loc(location), BaseName(baseName.str()),
+      : BaseName(baseName.str()),
         Arguments(std::move(arguments)) {}
 };
 
@@ -96,11 +76,10 @@ class CallableDeclaration : public Decl {
   std::unique_ptr<ParameterList> ReturnParameters;
 
 public:
-  CallableDeclaration(
-      const SourceLocation &location, llvm::StringRef name,
+  CallableDeclaration(llvm::StringRef name,
       Visibility visibility, std::unique_ptr<ParameterList> &&parameters,
       std::unique_ptr<ParameterList> &&returnParameters = nullptr)
-      : Decl(location, name, visibility), Parameters(std::move(parameters)),
+      : Decl(name, visibility), Parameters(std::move(parameters)),
         ReturnParameters(std::move(returnParameters)) {}
 };
 
@@ -116,14 +95,13 @@ class FunctionDefinition : public CallableDeclaration {
   bool Implemented;
 
 public:
-  FunctionDefinition(
-      const SourceLocation &location, llvm::StringRef name,
+  FunctionDefinition(llvm::StringRef name,
       Visibility visibility, StateMutability sm, bool isConstructor,
       std::unique_ptr<ParameterList> &&parameters,
       std::vector<std::unique_ptr<ModifierInvocation>> &&modifiers,
       std::unique_ptr<ParameterList> &&returnParameters,
       std::unique_ptr<Block> &&body)
-      : CallableDeclaration(location, name, visibility, std::move(parameters),
+      : CallableDeclaration(name, visibility, std::move(parameters),
                             std::move(returnParameters)),
         SM(sm), IsConstructor(isConstructor),
         FunctionModifiers(std::move(modifiers)), Body(std::move(body)),
@@ -132,13 +110,11 @@ public:
 
 class VariableDeclaration;
 class ParameterList {
-  SourceLocation Loc;
   std::vector<std::unique_ptr<VariableDeclaration>> Parameters;
 
 public:
-  ParameterList(const SourceLocation &location,
-                std::vector<std::unique_ptr<VariableDeclaration>> &&parameters)
-      : Loc(location), Parameters(std::move(parameters)) {}
+  ParameterList(std::vector<std::unique_ptr<VariableDeclaration>> &&parameters)
+      : Parameters(std::move(parameters)) {}
 };
 
 class VariableDeclaration : public Decl {
@@ -154,27 +130,25 @@ private:
   Location ReferenceLocation;
 
 public:
-  VariableDeclaration(const SourceLocation &sourceLocation,
-                      llvm::StringRef type, llvm::StringRef name,
+  VariableDeclaration(llvm::StringRef type, llvm::StringRef name,
                       std::unique_ptr<Expr> &&value, Visibility visibility,
                       bool isStateVar = false, bool isIndexed = false,
                       bool isConstant = false,
                       Location referenceLocation = Location::Unspecified)
-      : Decl(sourceLocation, name, visibility), TypeName(type),
+      : Decl(name, visibility), TypeName(type),
         Value(std::move(value)), IsStateVariable(isStateVar),
         IsIndexed(isIndexed), IsConstant(isConstant),
         ReferenceLocation(referenceLocation) {}
 };
 
 class ModifierInvocation {
-  SourceLocation Loc;
   std::string ModifierName;
   std::vector<std::unique_ptr<Expr>> Arguments;
 
 public:
-  ModifierInvocation(const SourceLocation &location, llvm::StringRef name,
+  ModifierInvocation(llvm::StringRef name,
                      std::vector<std::unique_ptr<Expr>> arguments)
-      : Loc(location), ModifierName(name), Arguments(std::move(arguments)) {}
+      : ModifierName(name), Arguments(std::move(arguments)) {}
 };
 
 } // namespace soll
