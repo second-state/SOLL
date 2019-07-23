8ae6ab6601df2aae5bba25eefc701854509c64b9
Yi Huang <yi@secondstate.io>
2019-07-23 10:38:59 +0000

    [ADT] Refactor: move helper template to STLExtras.

--- include/soll/AST/StmtVisitor.h
+++ include/soll/AST/StmtVisitor.h
@@ -1,17 +1,6 @@
 #pragma once
 
-namespace {
-
-/// Conditional const
-/// cond_const<true,  Type>: const Type
-/// cond_const<false, Type>: Type
-template <bool Const, class Type> struct cond_const {
-  typedef const Type type;
-};
-
-template <class Type> struct cond_const<false, Type> { typedef Type type; };
-
-} // namespace
+#include "soll/ADT/STLExtras.h"
 
 namespace soll {
 
