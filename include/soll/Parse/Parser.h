ef404534a504b1d1f5762e1eb20e8b8bff52d5cd
vincent <vincent@secondstate.io>
2019-07-24 15:43:50 +0800

    [CodingStyle] Don't use "using namespace" in header.

--- include/soll/Parse/Parser.h
+++ include/soll/Parse/Parser.h
@@ -2,8 +2,6 @@
 #include "soll/AST/AST.h"
 #include "soll/Basic/SourceManager.h"
 
-using namespace std;
-
 namespace soll {
 
 class Token;
@@ -53,50 +51,50 @@ private:
     llvm::StringRef Name;
     Decl::Visibility Vsblty = Decl::Visibility::Default;
     StateMutability SM = StateMutability::NonPayable;
-    unique_ptr<ParamList> Parameters;
-    vector<unique_ptr<ModifierInvocation>> Modifiers;
-    unique_ptr<ParamList> ReturnParameters;
+    std::unique_ptr<ParamList> Parameters;
+    std::vector<std::unique_ptr<ModifierInvocation>> Modifiers;
+    std::unique_ptr<ParamList> ReturnParameters;
   };
 
   // void parsePragmaVersion(langutil::SourceLocation const& _location,
-  // vector<Token> const& _tokens, vector<string> const&
+  // std::vector<Token> const& _tokens, std::vector<std::string> const&
   // _literals);
-  unique_ptr<PragmaDirective> parsePragmaDirective();
+  std::unique_ptr<PragmaDirective> parsePragmaDirective();
   ContractDecl::ContractKind parseContractKind();
-  unique_ptr<ContractDecl> parseContractDefinition();
+  std::unique_ptr<ContractDecl> parseContractDefinition();
   Decl::Visibility parseVisibilitySpecifier();
   StateMutability parseStateMutability();
   FunctionHeaderParserResult parseFunctionHeader(bool ForceEmptyName,
                                                  bool AllowModifiers);
-  unique_ptr<FunctionDecl> parseFunctionDefinitionOrFunctionTypeStateVariable();
-  unique_ptr<VarDecl> parseVariableDeclaration(
+  std::unique_ptr<FunctionDecl> parseFunctionDefinitionOrFunctionTypeStateVariable();
+  std::unique_ptr<VarDecl> parseVariableDeclaration(
       VarDeclParserOptions const &Options = {},
-      unique_ptr<Type> const &LookAheadArrayType = nullptr);
-  unique_ptr<Type> parseTypeNameSuffix(unique_ptr<Type> T);
-  unique_ptr<Type> parseTypeName(bool AllowVar);
-  unique_ptr<ParamList>
+      std::unique_ptr<Type> const &LookAheadArrayType = nullptr);
+  std::unique_ptr<Type> parseTypeNameSuffix(std::unique_ptr<Type> T);
+  std::unique_ptr<Type> parseTypeName(bool AllowVar);
+  std::unique_ptr<ParamList>
   parseParameterList(VarDeclParserOptions const &Options = {},
                      bool AllowEmpty = true);
-  unique_ptr<Block> parseBlock();
-  unique_ptr<Stmt> parseStatement();
-  unique_ptr<IfStmt> parseIfStatement();
-  unique_ptr<Stmt> parseSimpleStatement();
-  unique_ptr<DeclStmt> parseVariableDeclarationStatement(
-      unique_ptr<Type> const &LookAheadArrayType = make_unique<Type>());
-  unique_ptr<ExprStmt> parseExpressionStatement(
-      unique_ptr<Expr> &&PartiallyParsedExpression = nullptr);
-  unique_ptr<Expr>
-  parseExpression(unique_ptr<Expr> &&PartiallyParsedExpression = nullptr);
-  unique_ptr<Expr>
+  std::unique_ptr<Block> parseBlock();
+  std::unique_ptr<Stmt> parseStatement();
+  std::unique_ptr<IfStmt> parseIfStatement();
+  std::unique_ptr<Stmt> parseSimpleStatement();
+  std::unique_ptr<DeclStmt> parseVariableDeclarationStatement(
+      std::unique_ptr<Type> const &LookAheadArrayType = std::unique_ptr<Type>());
+  std::unique_ptr<ExprStmt> parseExpressionStatement(
+      std::unique_ptr<Expr> &&PartiallyParsedExpression = nullptr);
+  std::unique_ptr<Expr>
+  parseExpression(std::unique_ptr<Expr> &&PartiallyParsedExpression = nullptr);
+  std::unique_ptr<Expr>
   parseBinaryExpression(int MinPrecedence = 4,
-                        unique_ptr<Expr> &&PartiallyParsedExpression = nullptr);
-  unique_ptr<Expr>
-  parseUnaryExpression(unique_ptr<Expr> &&PartiallyParsedExpression = nullptr);
-  unique_ptr<Expr> parseLeftHandSideExpression(
-      unique_ptr<Expr> &&PartiallyParsedExpression = nullptr);
-  unique_ptr<Expr> parsePrimaryExpression();
-  vector<unique_ptr<Expr>> parseFunctionCallListArguments();
-  pair<vector<unique_ptr<Expr>>, vector<unique_ptr<string>>>
+                        std::unique_ptr<Expr> &&PartiallyParsedExpression = nullptr);
+  std::unique_ptr<Expr>
+  parseUnaryExpression(std::unique_ptr<Expr> &&PartiallyParsedExpression = nullptr);
+  std::unique_ptr<Expr> parseLeftHandSideExpression(
+      std::unique_ptr<Expr> &&PartiallyParsedExpression = nullptr);
+  std::unique_ptr<Expr> parsePrimaryExpression();
+  std::vector<std::unique_ptr<Expr>> parseFunctionCallListArguments();
+  std::pair<std::vector<std::unique_ptr<Expr>>, std::vector<std::unique_ptr<std::string>>>
   parseFunctionCallArguments();
 
   /// Used as return value of @see peekStatementType.
@@ -110,10 +108,10 @@ private:
   /// expression or to a type name. For this to be valid, path cannot be empty,
   /// but indices can be empty.
   struct IndexAccessedPath {
-    vector<unique_ptr<AST>> Path;
+    std::vector<std::unique_ptr<AST>> Path;
   };
 
-  pair<LookAheadInfo, IndexAccessedPath> tryParseIndexAccessedPath();
+  std::pair<LookAheadInfo, IndexAccessedPath> tryParseIndexAccessedPath();
 
   /// Performs limited look-ahead to distinguish between variable declaration
   /// and expression statement. For source code of the form "a[][8]"
@@ -124,18 +122,18 @@ private:
   /// @returns a typename parsed in look-ahead fashion from something like
   /// "a.b[8][2**70]", or an empty pointer if an empty @a _pathAndIncides has
   /// been supplied.
-  unique_ptr<Type>
+  std::unique_ptr<Type>
   typeNameFromIndexAccessStructure(IndexAccessedPath const &PathAndIndices);
   /// @returns an expression parsed in look-ahead fashion from something like
   /// "a.b[8][2**70]", or an empty pointer if an empty @a _pathAndIncides has
   /// been supplied.
-  unique_ptr<Expr>
+  std::unique_ptr<Expr>
   expressionFromIndexAccessStructure(IndexAccessedPath const &PathAndIndices);
-  unique_ptr<string> expectIdentifierToken();
-  unique_ptr<string> getLiteralAndAdvance();
+  std::unique_ptr<std::string> expectIdentifierToken();
+  std::unique_ptr<std::string> getLiteralAndAdvance();
   /// Creates an empty ParameterList at the current location (used if parameters
   /// can be omitted).
-  unique_ptr<AST> createEmptyParameterList();
+  std::unique_ptr<AST> createEmptyParameterList();
   llvm::StringRef getLiteralAndAdvance(llvm::Optional<Token> Tok);
 };
 
