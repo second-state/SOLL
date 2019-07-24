0e03d38d69d831aa8bafd736f9238467d69a2867
Yi Huang <yi@secondstate.io>
2019-07-24 08:51:59 +0000

    [AST] Fix one definition rule warning.

--- lib/AST/ASTPrinter.cpp
+++ lib/AST/ASTPrinter.cpp
@@ -4,19 +4,19 @@
 
 using namespace soll;
 
-void ASTPrinter::visit(const SourceUnitType &decl) {
+void ASTPrinter::visit(SourceUnitType &decl) {
   os() << indent() << "SourceUnit" << std::endl;
   ConstDeclVisitor::visit(decl);
   unindent();
 }
 
-void ASTPrinter::visit(const ContractDeclType &decl) {
+void ASTPrinter::visit(ContractDeclType &decl) {
   os() << indent() << "ContractDecl" << std::endl;
   ConstDeclVisitor::visit(decl);
   unindent();
 }
 
-void ASTPrinter::visit(const FunctionDecl &decl) {
+void ASTPrinter::visit(FunctionDeclType &decl) {
   os() << indent() << "FunctionDecl" << std::endl;
   ConstDeclVisitor::visit(decl);
   decl.getBody()->accept(*this);
@@ -29,49 +29,49 @@ void ASTPrinter::visit(ParamListType &param) {
   unindent();
 }
 
-void ASTPrinter::visit(const VarDecl &decl) {
+void ASTPrinter::visit(VarDeclType &decl) {
   os() << indent() << "VarDecl" << std::endl;
   ConstDeclVisitor::visit(decl);
   unindent();
 }
 
-void ASTPrinter::visit(const Block &block) {
+void ASTPrinter::visit(BlockType &block) {
   os() << indent() << "Block" << std::endl;
   ConstStmtVisitor::visit(block);
   unindent();
 }
 
-void ASTPrinter::visit(const UnaryOperator &op) {
+void ASTPrinter::visit(UnaryOperatorType &op) {
   os() << indent() << "UnaryOperator" << std::endl;
   ConstStmtVisitor::visit(op);
   unindent();
 }
 
-void ASTPrinter::visit(const BinaryOperator &op) {
+void ASTPrinter::visit(BinaryOperatorType &op) {
   os() << indent() << "BinaryOperator" << std::endl;
   ConstStmtVisitor::visit(op);
   unindent();
 }
 
-void ASTPrinter::visit(const Identifier &id) {
+void ASTPrinter::visit(IdentifierType &id) {
   os() << indent() << "Identifier" << std::endl;
   ConstStmtVisitor::visit(id);
   unindent();
 }
 
-void ASTPrinter::visit(const BooleanLiteral &literal) {
+void ASTPrinter::visit(BooleanLiteralType &literal) {
   os() << indent() << "BooleanLiteral" << std::endl;
   ConstStmtVisitor::visit(literal);
   unindent();
 }
 
-void ASTPrinter::visit(const StringLiteral &literal) {
+void ASTPrinter::visit(StringLiteralType &literal) {
   os() << indent() << "StringLiteral" << std::endl;
   ConstStmtVisitor::visit(literal);
   unindent();
 }
 
-void ASTPrinter::visit(const NumberLiteral &literal) {
+void ASTPrinter::visit(NumberLiteralType &literal) {
   os() << indent() << "NumberLiteral" << std::endl;
   ConstStmtVisitor::visit(literal);
   unindent();
