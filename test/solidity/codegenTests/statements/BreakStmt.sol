1b2763c58ac5d5f8a1b0c9948f58f6fda197280d
Lightbulb <lightbulb@yuyuko.moe>
2019-08-19 14:34:15 +0800

    [Test] add Break/ContinueStmt .sol test

---
+++ test/solidity/codegenTests/statements/BreakStmt.sol
@@ -0,0 +1,17 @@
+pragma solidity >0.4.0 <=0.7.0;
+
+contract C {
+  function BreakStmt() public {
+    int a;
+    while(a > 1) {
+      if (1 > a) {
+        if (3 > a) {
+          break;
+        }
+        a + 3;
+        break;
+      }
+      break;
+    }
+  }
+}
---
+++ test/solidity/codegenTests/statements/ContinueStmt.sol
@@ -0,0 +1,17 @@
+pragma solidity >0.4.0 <=0.7.0;
+
+contract C {
+  function ContinueStmt() public {
+    int a;
+    while(a > 1) {
+      if (1 > a) {
+        if (3 > a) {
+          continue;
+        }
+        a + 3;
+        continue;
+      }
+      continue;
+    }
+  }
+}
