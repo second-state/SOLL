eb3e58bb7f1be1b74512322ebb365bfefcba0d2c
Lightbulb <lightbulb@yuyuko.moe>
2019-08-28 14:26:40 +0800

    [Test] fix testcase operator.sol

--- test/solidity/codegenTests/operator.sol
+++ test/solidity/codegenTests/operator.sol
@@ -1,39 +1,69 @@
 pragma solidity ^ 0.5.0;
 
 contract Expr_LRValue {
-  function binary_operator(uint64 a, uint64 b) public pure returns(uint64) {
-    a = b + 1;
-    a = 2 - b;
-    a = b * 3;
-    a = b / 4;
-    a = b % 5;
+  function binary_operator_assignment() public pure {
+    // =, +=, -=, *=, /=, %=, <<=, >>=
+    uint64 a;
+    uint64 b;
+    a = b;
     a += b;
     a -= b;
     a *= 3;
     a /= b;
     a %= b;
-
-    1 && 1;
-    a && 1;
-    a || a;
-    1 || 1;
-
-    a << 2;
-    2 >> a;
-
-    2 | a;
-    2 & 2;
-
-    return a;
+    b <<= 10;
+    a >>= b;
+    a += b = a;
   }
-  function unary_operator() public pure returns(uint64) {
-    uint64 a = 1;
-    +a;
-    +1;
-    -a;
-    -1;
-    !a;
-    !1;
-    return a;
+  function binary_operator_arithmetic() public pure {
+    // +, -, *, /, %, <<, >>, ~
+    uint64 a;
+    uint64 b;
+    a + b;
+    a - b;
+    a * b;
+    a / b;
+    a % b;
+    a << b;
+    a >> b;
+    ~a;
+  }
+  function binary_operator_comparison() public pure {
+    // >, <, >=, <=, ==, !=
+    uint64 a;
+    uint64 b;
+    bool result;
+    result = a > b;
+    result = a < b;
+    result = a >= b;
+    result = a <= b;
+    result = a == b;
+    result = a != b;
+  }
+  function binary_operator_logical() public pure {
+    // ||, &&
+    bool a;
+    bool b;
+    a = a || b;
+    b = a && b;
+  }
+  function binary_operator_bitwise() public pure {
+    // &, |, ^
+    uint64 a;
+    uint64 b;
+    a = a & b;
+    b = a | b;
+    a = a ^ b;
+  }
+  function unary_operator() public pure {
+    // ++a, --a, a++, a--, -a, !a
+    uint64 a;
+    bool b;
+    a = ++a;
+    a = --a;
+    a = a++;
+    a = a--;
+    a = -a;
+    b = !b;
   }
 }
