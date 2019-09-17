8b6faed7de71498dd8bfb92b812461b6044068fb
Yi Huang <yi@secondstate.io>
2019-09-17 09:34:22 +0000

    [Frontend] Implement FuncSigPrinter.

--- lib/Frontend/ASTConsumers/FuncSigPrinter.cpp
+++ lib/Frontend/ASTConsumers/FuncSigPrinter.cpp
@@ -2,7 +2,23 @@
 #include "soll/AST/AST.h"
 #include "soll/AST/ASTConsumer.h"
 #include "soll/Frontend/ASTConsumers.h"
+#include <iomanip>
 #include <llvm/Support/raw_ostream.h>
+
+namespace {
+
+std::string ToString(const std::vector<unsigned char> &v,
+                     unsigned int size = 4) {
+  size = std::min(size, (unsigned int)v.size());
+
+  std::ostringstream oss;
+  for (unsigned int i = 0; i < size; ++i)
+    oss << std::hex << std::setw(2) << std::setfill('0')
+        << (unsigned int)(v[i]);
+  return oss.str();
+}
+
+} // namespace
 
 namespace soll {
 
@@ -18,6 +34,7 @@ public:
   }
 
   void visit(FunctionDeclType &) override;
+  void visit(ParamListType &) override;
 
   llvm::raw_ostream &os() { return Out; }
 };
@@ -26,8 +43,21 @@ std::unique_ptr<ASTConsumer> CreateFuncSigPrinter(llvm::raw_ostream &Out) {
   return std::make_unique<FuncSigPrinter>(Out);
 }
 
-void FuncSigPrinter::visit(FunctionDeclType &decl) {
-  // TODO
+void FuncSigPrinter::visit(FunctionDeclType &F) {
+  os() << ToString(F.getSignatureHash()) << ": " << F.getName() << "(";
+  F.getParams()->accept(*this);
+  os() << ")\n";
+}
+
+void FuncSigPrinter::visit(ParamListType &PL) {
+  bool first = true;
+  for (auto param : PL.getParams()) {
+    if (first)
+      first = false;
+    else
+      os() << ",";
+    os() << param->GetType()->getName();
+  }
 }
 
 } // namespace soll
