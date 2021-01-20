99f6d06a5e1bc69b03f31248f8838b0ebdab9070
eee4017 <eee4017@gmail.com>
2021-01-20 19:10:03 +0800

    [CI] move libsoll_runtime_test.so to tool and update github CI

--- test/soll-runtime-test/runtest.sh
+++ test/soll-runtime-test/runtest.sh
@@ -1,6 +1,6 @@
 SOLL_BASE="../.."
 
-cp ${SOLL_BASE}/build/utils/ssvm-evmc/test/soll-runtime-test/libsoll_runtime_test.so ${PWD}
+cp ${SOLL_BASE}/build/utils/ssvm-evmc/tools/soll-runtime-test/libsoll_runtime_test.so ${PWD}
 cp ${SOLL_BASE}/build/utils/ssvm-evmc/tools/ssvm-evmc/libssvm-evmc.so ${PWD}
 
 python soll-runtime-test.py
